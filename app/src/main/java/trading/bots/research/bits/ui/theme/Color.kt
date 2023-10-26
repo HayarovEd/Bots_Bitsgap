ab3ec554a3e99b6582a03cf62606d7bb2509b3fe
HayarovEd <edurd@rambler.ru>
2023-10-26 16:26:38 +0500

    3

--- app/src/main/java/trading/bots/research/bits/ui/theme/Color.kt
+++ app/src/main/java/trading/bots/research/bits/ui/theme/Color.kt
@@ -9,3 +9,32 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val background = Color(0xFF274AF2)
+val darkText = Color(0xFF000000)
+val white = Color(0xFFFFFFFF)
+
+val grey = Color(0xFFF6F6F6)
+val darkGrey = Color(0xFFD2DAFF)
+
+val greySub = Color(0xFFD6DDFF)
+val darkGreySub = Color(0xFFF5F7FF)
+
+val text1 = Color(0xFF0E2076)
+val subText1 = Color(0xFF274AF2)
+
+val text2 = Color(0xFF013E1D)
+val subText2 = Color(0xFF029747)
+
+val text3 = Color(0xFF44011D)
+val subText3 = Color(0xFFC20152)
+
+val text4 = Color(0xFF3F2405)
+val subText4 = Color(0xFFF06618)
+
+val text5 = Color(0xFF3F2405)
+val subText5 = Color(0xFFF08D18)
+
+
+
+
