b2b5cb2b5a2c705df04618b5ed44bf2aac799e52
HayarovEd <edurd@rambler.ru>
2023-10-27 09:45:27 +0500

    9

--- app/src/main/java/trading/bots/research/bits/ui/ItemPager.kt
+++ app/src/main/java/trading/bots/research/bits/ui/ItemPager.kt
@@ -4,18 +4,51 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun ItemPager(
     modifier: Modifier = Modifier,
-
+    strategies: List<Strategy>,
+    isThreeRow: Boolean,
+    name: String,
+    subName: String,
+    content: String,
+    fullContent: String,
+    baseTextColor: Color,
+    subTextColor: Color,
+    chip: Chip,
+    backgroundImagePath: Int,
 ) {
     val isShowFull = remember {
         mutableStateOf(false)
     }
     if (isShowFull.value) {
-
+        FullData(
+            name = name,
+            subName =subName,
+            content =fullContent,
+            baseTextColor = baseTextColor,
+            subTextColor = subTextColor,
+            chip = chip,
+            backgroundImagePath = backgroundImagePath,
+            onClick = {
+                isShowFull.value = false
+            }
+        )
     }  else {
-
+        ShortData(
+            strategies = strategies,
+            isThreeRow = isThreeRow,
+            name = name,
+            subName = subName,
+            content = content,
+            baseTextColor = baseTextColor,
+            subTextColor = subTextColor,
+            chip = chip,
+            backgroundImagePath = backgroundImagePath,
+            onClick = {
+                isShowFull.value = true
+            })
     }
 }
--- app/src/main/java/trading/bots/research/bits/ui/MockScreen.kt
+++ app/src/main/java/trading/bots/research/bits/ui/MockScreen.kt
@@ -1,13 +1,23 @@
 package trading.bots.research.bits.ui
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import trading.bots.research.bits.ui.theme.background
+import androidx.compose.ui.res.stringResource
+import trading.bots.research.bits.R
+import trading.bots.research.bits.ui.theme.subText1
+import trading.bots.research.bits.ui.theme.subText2
+import trading.bots.research.bits.ui.theme.subText3
+import trading.bots.research.bits.ui.theme.subText4
+import trading.bots.research.bits.ui.theme.subText5
+import trading.bots.research.bits.ui.theme.text1
+import trading.bots.research.bits.ui.theme.text2
+import trading.bots.research.bits.ui.theme.text3
+import trading.bots.research.bits.ui.theme.text4
+import trading.bots.research.bits.ui.theme.text5
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -25,11 +35,76 @@ fun MockScreen (
         state = pagerState
     ) { pageIndex ->
         when (pages[pageIndex]) {
-            MockState.Btd -> TODO()
-            MockState.Combo -> TODO()
-            MockState.DcaFuture -> TODO()
-            MockState.DcaTrading -> TODO()
-            MockState.Grid -> TODO()
+            MockState.Grid -> {
+                ItemPager(
+                    strategies = strategysGridTrading,
+                    isThreeRow = true,
+                    name = stringResource(id = R.string.grid_trading_bot),
+                    subName = stringResource(id = R.string.grid_trading_bot_sub),
+                    content = stringResource(id = R.string.grid_trading_bot_short),
+                    fullContent = stringResource(id = R.string.grid_trading_bot_full),
+                    baseTextColor = text1,
+                    subTextColor = subText1,
+                    chip = Chip.ChipOne,
+                    backgroundImagePath = R.drawable.bots_1
+                )
+            }
+            MockState.DcaTrading -> {
+                ItemPager(
+                    strategies = strategysDcaTrading,
+                    isThreeRow = false,
+                    name = stringResource(id = R.string.dca_trading_bot),
+                    subName = stringResource(id = R.string.dca_trading_bot_sub),
+                    content = stringResource(id = R.string.dca_trading_bot_short),
+                    fullContent = stringResource(id = R.string.dca_trading_bot_full),
+                    baseTextColor = text2,
+                    subTextColor = subText2,
+                    chip = Chip.ChipTwo,
+                    backgroundImagePath = R.drawable.bots_2
+                )
+            }
+            MockState.Btd -> {
+                ItemPager(
+                    strategies = strategysBtdTrading,
+                    isThreeRow = true,
+                    name = stringResource(id = R.string.btd_trading_bot),
+                    subName = stringResource(id = R.string.btd_trading_bot_sub),
+                    content = stringResource(id = R.string.btd_trading_bot_short),
+                    fullContent = stringResource(id = R.string.btd_trading_bot_full),
+                    baseTextColor = text3,
+                    subTextColor = subText3,
+                    chip = Chip.ChipThree,
+                    backgroundImagePath = R.drawable.bots_3
+                )
+            }
+            MockState.DcaFuture -> {
+                ItemPager(
+                    strategies = strategysDcaFutureTrading,
+                    isThreeRow = false,
+                    name = stringResource(id = R.string.dca_futures_trading_bot),
+                    subName = stringResource(id = R.string.dca_futures_trading_bot_sub),
+                    content = stringResource(id = R.string.dca_futures_trading_bot_short),
+                    fullContent = stringResource(id = R.string.dca_futures_trading_bot_full),
+                    baseTextColor = text4,
+                    subTextColor = subText4,
+                    chip = Chip.ChipTwo,
+                    backgroundImagePath = R.drawable.bots_4
+                )
+            }
+            MockState.Combo -> {
+                ItemPager(
+                    strategies = strategysComboTrading,
+                    isThreeRow = true,
+                    name = stringResource(id = R.string.combo_trading_bot),
+                    subName = stringResource(id = R.string.combo_trading_bot_sub),
+                    content = stringResource(id = R.string.combo_trading_bot_short),
+                    fullContent = stringResource(id = R.string.combo_trading_bot_full),
+                    baseTextColor = text5,
+                    subTextColor = subText5,
+                    chip = Chip.ChipTwo,
+                    backgroundImagePath = R.drawable.bots_5
+                )
+            }
         }
     }
 }
